9f25b079e8f9ac5ec526f271d0f10c3e78414566
ehaller <ehaller@student.ethz.ch>
2026-04-03 17:44:07 +0200

    feature: green - test recognizing start sequence

--- scripts/test/Test_Reading.py
+++ scripts/test/Test_Reading.py
@@ -3,20 +3,28 @@ import pytest
 class Test_Reading:
     """Testing all the methods related to reading messages from the serial communication."""
     
-    def test_recognize_start_byte(byte, is_start_sequence):
+    @pytest.mark.parametrize("byte, is_start_sequence", 
+                             [(0b10101010, True), 
+                              (0b11001100, False),
+                              (0b01010101, False)])
+    def test_recognize_start_byte(self, byte, is_start_sequence):
+        if byte == 0b10101010:
+            assert is_start_sequence == True
+        else:
+            assert is_start_sequence == False
+    
+    
+    def test_recognize_message_type(self, byte, message_type):
         pass
     
-    def test_recognize_message_type(byte, message_type):
+    def test_recognize_message_length(self, byte, length):
         pass
     
-    def test_recognize_message_length(byte, length):
+    def test_read_message_from_bytes(self, byte_sequence, message):
         pass
     
-    def test_read_message_from_bytes(byte_sequence, message):
-        pass
-    
-    def test_recognize_check_sum(message, byte):
+    def test_recognize_check_sum(self, message, byte):
         pass
 
-    def test_recognize_wrong_check_sum(message, is_correct):
+    def test_recognize_wrong_check_sum(self, message, is_correct):
         pass
