a0ceccf9aaf36c2abeb202b73b939c86495184c7
ehaller <ehaller@student.ethz.ch>
2026-04-05 18:12:09 +0200

    feature: Message  type enum with byte values

---
+++ scripts/SerialComPy/MessageType.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    "All the different message types as Enums and the corresponding values"
+    WRITE_DIGITAL = 0x01
+    READ_DIGITAL = 0x02
+    DIGITAL_VALUE_RESPONSE = 0x03
+    WRITE_ANALOGUE = 0x04
+    READ_ANALOGUE = 0x05
+    ANALOGUE_VALUE_rESPONSE = 0x06
+    WRITING_CONFIRMATION = 0x07
+    RESET = 0x10
